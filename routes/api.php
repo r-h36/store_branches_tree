70ad5c5048e871ccfbda89a8d417c862002367ec
r-h36 <lh.ray106@gmail.com>
2019-01-13 11:29:09 +1100

    Setup branch API routes

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,20 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * 
+ */
+Route::get('/branches', 'BranchController@index')->name('branch.index');
+Route::get('/branches/{id}', 'BranchController@view')->name('branch.view');
+Route::get('/branches/{id}/with-children', 'BranchController@viewWithChildren')->name('branch.viewWithChildren');
+
+Route::post('/branches', 'BranchController@create')->name('branch.create');
+Route::patch('/branches/{id}', 'BranchController@update')->name('branch.update');
+Route::delete('/branches/{id}', 'BranchController@delete')->name('branch.delete');
+Route::post('/branches/{id}/move-to/{toBranchId}', 'BranchController@move')->name('branch.move');
